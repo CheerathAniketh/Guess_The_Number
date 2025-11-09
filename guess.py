5060894fa1862b6274ae016eb48f553aecffef31
Aniketh <168458598+CheerathAniketh@users.noreply.github.com>
2025-11-09 07:15:15 +0530

    Fix typos and improve input prompts in guess.py

--- guess.py
+++ guess.py
@@ -2,27 +2,20 @@ import random
 
 def guess():
     print("Welcome to Guess the number game!")
+    print("Rules of this game is simple you need to guess a number between 0-20 and enter that below")
 
     computer = random.choice(range(21))
+    print("Type your guess below")
 
 
     while True:
-        user = int(input("Guess the number "))
-
+        user = int(input("Number "))
         if user == computer:
-            print("Success you've guessed it rigth ")
+            print("Success you've guessed it right ")
             break
-                 
         elif user < computer:
             print("Try some higer numbers")
-            
-
         elif user > computer:
             print("Not that high try some lower numbers")
             
-        
-        
-        
-
-
 guess()
